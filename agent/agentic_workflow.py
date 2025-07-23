7abcdda79c55b8f5a82d3da853c65d7bcc53a46a
Naga Vinay <nagavinay0905@gmail.com>
2025-07-23 11:12:12 +0530

    New commit because its a new date, starting on clean slate

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -2,19 +2,35 @@ from utils.model_loaders import ModelLoader
 from prompt_library.prompt import SYSTEM_PROMPT
 from langgraph.graph import StateGraph, MessagesState, END, START
 from langgraph.prebuilt import ToolNode, tools_condition
-# from tools.weather_info_tool import WeatherInfoTool
-# from tools.place_search_tool import PlaceSearchTool
-# from tools.expense_calculator_tool import CalculatorTool
-# from tools.currency_conversion_tool import CurrencyConverterTool
+from tools.weather_info_tool import WeatherInfoTool
+from tools.place_search_tool import PlaceSearchTool
+from tools.expense_calculator_tool import CalculatorTool
+from tools.currency_conversion_tool import CurrencyConverterTool
 
 
 
 class GraphBuilder():
-    def __init__(self):
-       self.tools=[
-           
-       ]
-       self.system_prompt=SYSTEM_PROMPT
+    def __init__(self,model_provider:str = "groq"):
+        self.model_loader = ModelLoader(model_provider=model_provider)
+        self.llm = self.model_loader.load_llm()
+        
+        self.tools = []
+        
+        self.weather_tools = WeatherInfoTool()
+        self.place_search_tools = PlaceSearchTool()
+        self.calculator_tools = CalculatorTool()
+        self.currency_converter_tools = CurrencyConverterTool()
+        
+        self.tools.extend([* self.weather_tools.weather_tool_list, 
+                           * self.place_search_tools.place_search_tool_list,
+                           * self.calculator_tools.calculator_tool_list,
+                           * self.currency_converter_tools.currency_converter_tool_list])
+        
+        self.llm_with_tools = self.llm.bind_tools(tools=self.tools)
+        
+        self.graph = None
+        
+        self.system_prompt=SYSTEM_PROMPT
     
 
 
@@ -32,20 +48,17 @@ class GraphBuilder():
 
 
     def build_graph(self):
+        
         graph_builder=StateGraph(MessagesState)
-
-        graph_builder.add_edge(START,self.agent_function)
-
-        graph_builder.add_node("agent",ToolNode(tools=self.tools))
-
-        graph_builder.add_conditional_edges("agent",tools_condition) 
-
+        graph_builder.add_node("agent", self.agent_function)
+        graph_builder.add_node("tools", ToolNode(tools=self.tools))
+        graph_builder.add_edge(START,"agent")
+        graph_builder.add_conditional_edges("agent",tools_condition)
         graph_builder.add_edge("tools","agent")
-
         graph_builder.add_edge("agent",END)
-
-        self.graph=graph_builder.compile()
+        self.graph = graph_builder.compile()
         return self.graph
+        
 
     def __call__(self):
         return self.build_graph()
