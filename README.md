a1b4e0166247709b8e5ec43d42509c2ea16ef2a8
Naga Vinay <nagavinay0905@gmail.com>
2025-07-23 13:34:39 +0530

    The final commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# uv pip list
+# to run the server : streamlit run AI_TRIPPLANNER\streamlit_app.py
+
+
+### For Server :- 
+## first change to the cd W:\Youtube_langraph1\AI_TRIPPLANNER
+## then:-
+## "uvicorn main:app --host 0.0.0.0 --port 8000 --reload" run this after changing to this path W:\Youtube_langraph1\AI_TRIPPLANNER
+### for running streamlitt app :- " streamlit run streamlit_app.py "
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,40 @@
 import streamlit as st
-import datetime
 import requests
+import datetime
+
+# from exception.exceptions import TradingBotException
 import sys
 
-BASE_URL = "https://localhost:8000"
+BASE_URL = "http://localhost:8000"  # Backend endpoint
 
 st.set_page_config(
-    page_title=" TRAVEL PLANNER AGENTIC APPLICATION",
-    page_icon="🌏",
+    page_title="🌍 Travel Planner Agentic Application",
+    page_icon="🌍",
     layout="centered",
     initial_sidebar_state="expanded",
 )
 
-st.title("🌏 Travel Planner Agentic Application")
+st.title("🌍 Travel Planner Agentic Application")
+
+# Initialize chat history
 if "messages" not in st.session_state:
-    st.session_state.messages=[]
+    st.session_state.messages = []
 
-st.header(" How can I help you in planning a trip? Let me know where do you want to Visit!! ")
+# Display chat history
+st.header("How can I help you in planning a trip? Let me know where do you want to visit.")
 
-with st.form(key="query_form",clear_on_submit=True):
-
+# Chat input box at bottom
+with st.form(key="query_form", clear_on_submit=True):
     user_input = st.text_input("User Input", placeholder="e.g. Plan a trip to Goa for 5 days")
     submit_button = st.form_submit_button("Send")
 
 if submit_button and user_input.strip():
     try:
-        with st.spinner("Bot is thinking...."):
-            payload={"question":user_input}
-            response=requests.post(f"{BASE_URL}/query",json=payload)
+        # # Show user message
+        # Show thinking spinner while backend processes
+        with st.spinner("Bot is thinking..."):
+            payload = {"question": user_input}
+            response = requests.post(f"{BASE_URL}/query", json=payload)
 
         if response.status_code == 200:
             answer = response.json().get("answer", "No answer returned.")
